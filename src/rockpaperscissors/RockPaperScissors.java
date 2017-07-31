5f5824062dc59b92e4f21e0252aadd56c0f448d5
Matthew <matthew_mo@hotmail.co.uk>
2017-07-31 15:18:44 +0100

    First draft of code

---
+++ src/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rockpaperscissors;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ *
+ * @author matth
+ */
+public class RockPaperScissors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Starting game of rock, paper and scissors...");
+        System.out.println("Please choose either rock, paper or scissors: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int min = 0;
+        int max = 2;
+        
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);     
+        String randomFeature = "";
+        
+        switch (randomNum)
+        {
+            case 0:
+                randomFeature = "rock";
+                break;
+            case 1:
+                randomFeature = "paper";
+                break;
+            case 2:
+                randomFeature = "scissors";
+                break;
+        }
+        
+        System.out.printf("You appear to be battling against %s%n", randomFeature);
+        
+        switch(input)
+        {
+            case "rock":
+                switch(randomFeature)
+                {
+                    case "rock":
+                        System.out.println("No winner");
+                        break;
+                    case "paper":
+                        System.out.println("You lose!");
+                        break;
+                    case "scissors":
+                        System.out.println("You win!");
+                        break; 
+                }
+                break;
+            case "paper":
+                switch(randomFeature)
+                {
+                    case "rock":
+                        System.out.println("You win!");
+                        break;
+                        
+                    case "paper":
+                        System.out.println("No winner");
+                        break;
+                        
+                    case "scissors":
+                        System.out.println("You lose!");
+                        break;
+                }
+                break;
+            case "scissors":
+            {
+                switch(randomFeature)
+                {
+                    case "rock":
+                        System.out.println("You lose!");
+                        break;
+                    case "paper":
+                        System.out.println("You win!");
+                        break;
+                    case "scissors":
+                        System.out.println("No winner");
+                        break;
+                }
+                break;
+            }
+        }
+    }
+}
